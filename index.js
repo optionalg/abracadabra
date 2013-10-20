78da5feac8b97d2273fe4e2d3a1d0e8da01df309
Nicholas Kinsey <pyro@feisty.io>
2013-10-20 18:29:07 +1000

    bare

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-(function() {
-  var spawn;
+var spawn;
 
-  spawn = require('child_process').spawn;
+spawn = require('child_process').spawn;
 
-  module.exports = function(args) {
-    var cmd, message, title;
-    if (args == null) {
-      args = {};
-    }
-    cmd = 'terminal-notifier.app/Contents/MacOS/terminal-notifier';
-    cmd = "" + __dirname + "/" + cmd;
-    title = args.title, message = args.message;
-    return spawn(cmd, ['-title', title, '-message', message]);
-  };
-
-}).call(this);
+module.exports = function(args) {
+  var cmd, message, title;
+  if (args == null) {
+    args = {};
+  }
+  cmd = 'terminal-notifier.app/Contents/MacOS/terminal-notifier';
+  cmd = "" + __dirname + "/" + cmd;
+  title = args.title, message = args.message;
+  return spawn(cmd, ['-title', title, '-message', message]);
+};
